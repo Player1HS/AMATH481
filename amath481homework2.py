af9a3e60800fab2b76e3eb4dfe48e3bb88ebc370
player1hs <harshil.sharma2004@gmail.com>
2024-10-18 19:04:10 -0700

    hw2 complete

--- amath481homework2.py
+++ amath481homework2.py
@@ -7,35 +7,34 @@ def shoot(phi_primes, x, epsilon):
 
 tol = 1e-6  # define a tolerance level 
 col = ['r', 'b', 'g', 'c', 'm']  # eigenfunc colors
-epsilon_0 = 1; A = 1; initial = [0, A]
-xspan =  np.arange(-4, 4, 0.1)
+epsilon_0 = 0.1; initial = [1, 4]
+xspan =  np.arange(-4, 4.1, 0.1)
 
 eigenvals=[]
-eigenfuncs=[]
+A1 = np.zeros((len(xspan), 5))
 
 for modes in range(1, 6):  # begin mode loop
     epsilon = epsilon_0  
     depsilon = epsilon_0 / 100
     for _ in range(1000):
         phi = odeint(shoot, initial, xspan, args=(epsilon,)) 
+        boundcheck = phi[-1, 1] + np.sqrt(16 - epsilon)*phi[-1, 0]
 
         if abs(phi[-1, 0]) < tol:  # check for convergence
             break  # get out of convergence loop
 
-        if (-1) ** (modes + 1) * phi[-1, 0] > 0:
+        if (-1) ** (modes + 1) * boundcheck > 0:
             epsilon += depsilon
         else:
             epsilon -= depsilon / 2
             depsilon /= 2
-
+    
     eigenvals.append(epsilon)
     epsilon_0 = epsilon + 0.1  # after finding eigenvalue, pick new start
     norm = np.trapz(phi[:, 0] * phi[:, 0], xspan)  # calculate the normalization
     plt.plot(xspan, phi[:, 0] / np.sqrt(norm), col[modes - 1])  # plot modes
-    eigenfuncs.append(phi[:, 0] / np.sqrt(norm))
+    A1[:, modes - 1] = np.abs(phi[:, 0] / np.sqrt(norm))
 
 eigenvals = np.array(eigenvals)
-eigenfuncs = np.array(eigenfuncs)
-A1 = np.transpose(eigenfuncs)
 A2 = eigenvals
 plt.show()
